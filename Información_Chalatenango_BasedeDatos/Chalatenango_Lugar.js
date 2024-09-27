fc04fe363b19f20dcfc6398b868e1936746277d6
Rodrigo José Miranda Hurtado <rodrigomiranda091@gmail.com>
2024-09-26 18:54:40 -0600

    Agregando Chalatenango_Lugar.js e Información_detallada.js a la carpeta de información.

---
+++ Información_Chalatenango_BasedeDatos/Chalatenango_Lugar.js
@@ -0,0 +1,79 @@
+[
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Cerro El Pital.",
+    "location": "https://maps.app.goo.gl/xu25GKGWwuNSiTMz9",
+    "description": "Es el punto más alto de El Salvador, con una altitud de 2,730 metros sobre el nivel del mar. Ofrece vistas espectaculares y es ideal para acampar, hacer senderismo y disfrutar de la naturaleza. En días despejados, puedes ver hasta el océano Pacífico desde la cima."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Cerro Miramundo.",
+    "location": "https://maps.app.goo.gl/rhUitCfMjHPPLZZv5",
+    "description": "Visita Miramundo, un pintoresco pueblo de montaña con impresionantes vistas panorámicas y un clima fresco, ideal para disfrutar de la tranquilidad y la belleza natural."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Embalse de Cerrón Grande.",
+    "location": "https://maps.app.goo.gl/SpnA2AEjfBzuX3cd8",
+    "description": "También conocido como el Lago Suchitlán, es el cuerpo de agua artificial más grande de El Salvador. Aquí puedes hacer paseos en lancha y observar aves, ya que es un área rica en biodiversidad."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Parque Central de Chalatenango.",
+    "location": "https://maps.app.goo.gl/cyfdeKua2Xk9cvp79",
+    "description": "Disfruta de un paseo por el Parque Central de Chalatenango, un espacio verde lleno de vida y color, donde podrás disfrutar de la arquitectura colonial y la hospitalidad de los lugareños."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Parque La Palma",
+    "location": "https://maps.app.goo.gl/fNwP9HiFkYwfTwYb9",
+    "description": "Un pueblo pintoresco conocido por su arte naif, que es un estilo de pintura colorido y sencillo. La Palma es un centro cultural importante donde puedes comprar artesanías y ver murales que decoran el pueblo."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Hotel Entre Pinos.",
+    "location": "https://maps.app.goo.gl/yXMbew7L1tk9cKEL6",
+    "description": "Es un amplio hotel con instalaciones acogedoras que le garantizaran un descanso formidable, cuenta con paquetes diseñados para disfrutar una visita de día o alojamiento prolongado con las mayores comodidades de un resort."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Río Sumpul",
+    "location": "https://maps.app.goo.gl/yKkfmWhnNRQbzkZG7",
+    "description": "Disfruta de un refrescante baño en el Río Sumpul, un hermoso río rodeado de exuberante vegetación y cascadas, ideal para disfrutar de la naturaleza y relajarse."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Turicentro Agua Fría.",
+    "location": "https://maps.app.goo.gl/NoLT99ttPn1Yqr969",
+    "description": "El Turicentro Agua Fría está ubicado en la parte urbana de la ciudad de Chalatenango, fue construido en 1977 como un centro recreativo familiar. Es un lugar muy visitado, especialmente en épocas de vacaciones o festivos."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "La Palma Chalatenango.",
+    "location": "https://maps.app.goo.gl/F9XLjogCmeeHy3gi9",
+    "description": "El Pueblo de la Palma Posee una dimensión territorial de 13,189 kilpometros cuadrados y está bordeando al norte de San Ignacio. La Palma es una zona montañosa y esta conformado por 8 cantones, 87 caseríos y 6 barrios."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "San Ignacio",
+    "location": "https://maps.app.goo.gl/MiKuzJpkaGEE3CT3A",
+    "description": "Un pequeño pueblo cercano a El Pital, conocido por su ambiente tranquilo y sus vistas impresionantes. Es un buen punto de partida para explorar la región."
+  },
+  {
+    "country": "El Salvador",
+    "city": "Chalatenango",
+    "name": "Parque Acuático Paraíso de la Montaña",
+    "location": "https://maps.app.goo.gl/vPx2AqB7wrMMopwQ9",
+    "description": "Parque Acuático Paraíso de la Montaña es un parque acuático en Chalatenango, El Salvador, que ofrece actividades para toda la familia, como piscinas y toboganes."
+  }
+]
---
+++ Información_Chalatenango_BasedeDatos/Información_detallada.js
@@ -0,0 +1,91 @@
+[
+    {
+      "place_id": 1,
+      "direction": "https://maps.app.goo.gl/xu25GKGWwuNSiTMz9",
+      "best_season_to_go": "diciembre - marzo.",
+      "where_to_stay": "Hotel entre Pinos, Nanda Parbat Hostal, Hotel de Montaña El Pital Highland.",
+      "where_to_eat": "La Cafetera, Baleadas Casa Lindo, La Placita Restaurant.",
+      "safety_tips": "Antes de emprender una ruta de senderismo, debe informarse sobre las condiciones climáticas y el estado de los senderos, Notificar a alguien sobre el recorrido, evitar caminar solo y mantenerse en grupo, respetar la flora y la fauna y seguir las indicaciones de los guías locales."
+    },
+    {
+      "place_id": 2,
+      "direction": "https://maps.app.goo.gl/rhUitCfMjHPPLZZv5",
+      "best_season_to_go": "diciembre - marzo.",
+      "where_to_stay": "Hostal Miramundo, El Pital Highland, La Posada del Cielo, Hacienda Los Dos Sauces.",
+      "where_to_eat": "La Placita Restaurant, Villa Café, Restaurante Rancho Escondido.",
+      "safety_tips": "Se recomienda hacer una planificación adecuada, contar con un equipo y la vestimenta adecuada, evitar dañar la flora y la fauna, considerar las condiciones climáticas y seguir las indicaciones de los guías locales."
+    },
+    {
+      "place_id": 3,
+      "direction": "https://maps.app.goo.gl/SpnA2AEjfBzuX3cd8",
+      "best_season_to_go": "septiembre - abril.",
+      "where_to_stay": "Casa 1800 Suchitoto, Los Almendros de San Lorenzo, Hostal Koltín Suchitoto, Casa Hotel Santa Lucía.",
+      "where_to_eat": "Casa 1800 Suchitoto, Restaurante el Zope, Brutto - El Salvador.",
+      "safety_tips": "Respetar la flora y fauna del lugar, no contaminar el lago y seguir las indicaciones de los guías locales."
+    },
+    {
+      "place_id": 4,
+      "direction": "https://maps.app.goo.gl/cyfdeKua2Xk9cvp79",
+      "best_season_to_go": "Durante todo el año.",
+      "where_to_stay": "Hotel Jardín De Las Marias, The Mayan Grouper, El Tejado.",
+      "where_to_eat": "Vidamia Restaurante y Café, Peñaflor Restaurante, Olan Café.",
+      "safety_tips": "Infórmate sobre el área, visita durante el día, conoce los servicios de emergencia."
+    },
+    {
+      "place_id": 5,
+      "direction": "https://maps.app.goo.gl/fNwP9HiFkYwfTwYb9",
+      "best_season_to_go": "noviembre - abril.",
+      "where_to_stay": "Hostal Posada Real, Entre Pinos Hotel, Hotel La Palma.",
+      "where_to_eat": "Olan Café, Villa Café.",
+      "safety_tips": "Visitar durante el día, Conoce los servicios de emergencia, cuida las estructuras."
+    },
+    {
+      "place_id": 6,
+      "direction": "https://maps.app.goo.gl/yXMbew7L1tk9cKEL6",
+      "best_season_to_go": "Durante todo el año.",
+      "where_to_stay": "Hotel Entre Pinos, La Posada de Suchitlan, EL Pital Ecolodge",
+      "where_to_eat": "La Placita Restaurant, La Cafeta, Olan Café.",
+      "safety_tips": "Ten precaución al salir, Comunica tus planes, usa el transporte recomendado."
+    },
+    {
+      "place_id": 7,
+      "direction": "https://maps.app.goo.gl/yKkfmWhnNRQbzkZG7",
+      "best_season_to_go": "Durante todo el año.",
+      "where_to_stay": "La Posada de Suchitlán, Hostal Taíces de Mi Pueblo, Hotel y Restaurante LOS AMIGOS.",
+      "where_to_eat": "Hotel y Restaurante LOS AMIGOS, Turicentro Rio Sumpul, Rancho Bambú.",
+      "safety_tips": "Investiga las condiciones del río, usa equipo adecuado para actividades acuáticas, Mantente alejado de las orillas resbaladizas y lleva un botiquín de primeros auxilios."
+    },
+    {
+      "place_id": 8,
+      "direction": "https://maps.app.goo.gl/NoLT99ttPn1Yqr969",
+      "best_season_to_go": "Durante todo el año.",
+      "where_to_stay": "Hotel y Restaurante LOS AMIGOS, La Posada de Suchitlán, Hotel y Restaurante El Tejado.",
+      "where_to_eat": "Hotel y Restaurante LOS AMIGOS, Hotel y Restaurante El Tejado.",
+      "safety_tips": "Informate sobre el lugar, sus horarios y actividades, ten un uso adecuado de las atracciones."
+    },
+    {
+      "place_id": 9,
+      "direction": "https://maps.app.goo.gl/F9XLjogCmeeHy3gi9",
+      "best_season_to_go": "diciembre - abril.",
+      "where_to_stay": "Hotel La Palma, Hostal Posada Real, Entre Pinos Hotel.",
+      "where_to_eat": "La Placita Restaurant, Olan Café, Rocios Ice Cream.",
+      "safety_tips": "Investiga el lugar, evita zonas peligrosas, informate sobre horarios y actividades."
+    },
+    {
+      "place_id": 10,
+      "direction": "https://maps.app.goo.gl/MiKuzJpkaGEE3CT3A",
+      "best_season_to_go": "Durante todo el año.",
+      "where_to_stay": "Entre Pinos Hotel, Las Tilapias Hotel y Restaurante, Cabañas El Pital.",
+      "where_to_eat": "Las Tilapias Hotel y Restaurante, Restaurante Rancho Escondido, Romeros Cocina y Cafe.",
+      "safety_tips": "Informate del lugar, respeta la flora y la fauna, ten un equipo y vestuario adecuados para caminatas, montañismo entre otras actividades."
+    },
+    {
+      "place_id": 11,
+      "direction": "https://maps.app.goo.gl/vPx2AqB7wrMMopwQ9",
+      "best_season_to_go": "Durante todo el año.",
+      "where_to_stay": "Hotel y Restaurante LOS AMIGOS, Las Terrazas del Alto, La Posada de Suchitlán.",
+      "where_to_eat": "Hotel y Restaurante LOS AMIGOS, Restaurante El Paraíso, Hostal y Restaurante Paraíso de la montaña.",
+      "safety_tips": "Supervisa a los menores, uso adecuado de las atracciones, respeta las señales de seguridad."
+    }
+  ]
+  
