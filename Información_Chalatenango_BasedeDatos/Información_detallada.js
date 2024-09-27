2995ecfea7d6adf67e9d70c51a642dd723a9ce47
MH220744 <127071729+MH220744@users.noreply.github.com>
2024-09-26 21:57:44 -0600

    Corrigiendo Información_detallada.js
    Corrigiendo las direcciones de cada lugar turístico.

--- Información_Chalatenango_BasedeDatos/Información_detallada.js
+++ Información_Chalatenango_BasedeDatos/Información_detallada.js
@@ -1,7 +1,7 @@
 [
     {
       "place_id": 1,
-      "direction": "https://maps.app.goo.gl/xu25GKGWwuNSiTMz9",
+      "direction": "Desde la terminal de Oriente en San Salvador, toma la ruta 119 hacia San Ignacio, luego toma la ruta 509 hasta el cantón Río Chiquito. Desde allí, camina 5 kilómetros o contrata un pick-up que cuesta entre $25 y $30 para llegar al Cerro El Pital.",
       "best_season_to_go": "diciembre - marzo.",
       "where_to_stay": "Hotel entre Pinos, Nanda Parbat Hostal, Hotel de Montaña El Pital Highland.",
       "where_to_eat": "La Cafetera, Baleadas Casa Lindo, La Placita Restaurant.",
@@ -9,7 +9,7 @@
     },
     {
       "place_id": 2,
-      "direction": "https://maps.app.goo.gl/rhUitCfMjHPPLZZv5",
+      "direction": "Toma la ruta 119 en la terminal de oriente en San Salvador hacia San Ignacio, dirígete a la terminal de buses a Las Pilas y, si tomas el bus a Las Pilas, bájate en la parada del cantón Río Chiquito y camina hasta tu destino.",
       "best_season_to_go": "diciembre - marzo.",
       "where_to_stay": "Hostal Miramundo, El Pital Highland, La Posada del Cielo, Hacienda Los Dos Sauces.",
       "where_to_eat": "La Placita Restaurant, Villa Café, Restaurante Rancho Escondido.",
@@ -17,7 +17,7 @@
     },
     {
       "place_id": 3,
-      "direction": "https://maps.app.goo.gl/SpnA2AEjfBzuX3cd8",
+      "direction": "Desde San Salvador, toma la autopista a Santa Ana, sigue las señales hacia Chalatenango, y al llegar, toma la carretera señalizada hacia el embalse. Continúa siguiendo las indicaciones hasta llegar al Embalse Cerrón Grande.",
       "best_season_to_go": "septiembre - abril.",
       "where_to_stay": "Casa 1800 Suchitoto, Los Almendros de San Lorenzo, Hostal Koltín Suchitoto, Casa Hotel Santa Lucía.",
       "where_to_eat": "Casa 1800 Suchitoto, Restaurante el Zope, Brutto - El Salvador.",
@@ -25,7 +25,7 @@
     },
     {
       "place_id": 4,
-      "direction": "https://maps.app.goo.gl/cyfdeKua2Xk9cvp79",
+      "direction": "Desde el Boulevard Arturo Castellanos, toma el Boulevard Venezuela hacia la Alameda Manuel Enrique Araujo. Luego, conduce por la carretera Troncal del Norte hasta Chalatenango. Finalmente, toma la carretera 3E hasta llegar a la Calle San Martín.",
       "best_season_to_go": "Durante todo el año.",
       "where_to_stay": "Hotel Jardín De Las Marias, The Mayan Grouper, El Tejado.",
       "where_to_eat": "Vidamia Restaurante y Café, Peñaflor Restaurante, Olan Café.",
@@ -33,7 +33,7 @@
     },
     {
       "place_id": 5,
-      "direction": "https://maps.app.goo.gl/fNwP9HiFkYwfTwYb9",
+      "direction": "Toma la ruta de buses 119 desde la terminal de Oriente en San Salvador. Esta ruta va hasta la Frontera El Poy en Citalá y pasa por los municipios de La Palma y San Ignacio.",
       "best_season_to_go": "noviembre - abril.",
       "where_to_stay": "Hostal Posada Real, Entre Pinos Hotel, Hotel La Palma.",
       "where_to_eat": "Olan Café, Villa Café.",
@@ -41,7 +41,7 @@
     },
     {
       "place_id": 6,
-      "direction": "https://maps.app.goo.gl/yXMbew7L1tk9cKEL6",
+      "direction": "Si viajas en automóvil, dirígete al kilómetro 87.5 de la Carretera Troncal del Norte en San Ignacio. Si viajas en autobús, toma la ruta 119 desde la Terminal de Oriente en San Salvador, el hotel está unos kilómetros después de La Palma. Pide al personal que te indique donde bajar.",
       "best_season_to_go": "Durante todo el año.",
       "where_to_stay": "Hotel Entre Pinos, La Posada de Suchitlan, EL Pital Ecolodge",
       "where_to_eat": "La Placita Restaurant, La Cafeta, Olan Café.",
@@ -49,7 +49,7 @@
     },
     {
       "place_id": 7,
-      "direction": "https://maps.app.goo.gl/yKkfmWhnNRQbzkZG7",
+      "direction": "Debes tomar la ruta de buses 119 por la terminal de oriente en San Salvador, al llegar a San Ignacio debes bajar en el punto de buses de la ruta 509A, luego toma la ruta 509A que conduce a cantón Las Pilas. Al llegar busca quien te indique claramente que lugares existen para poder visitar el rió Sumpul.",
       "best_season_to_go": "Durante todo el año.",
       "where_to_stay": "La Posada de Suchitlán, Hostal Taíces de Mi Pueblo, Hotel y Restaurante LOS AMIGOS.",
       "where_to_eat": "Hotel y Restaurante LOS AMIGOS, Turicentro Rio Sumpul, Rancho Bambú.",
@@ -57,7 +57,7 @@
     },
     {
       "place_id": 8,
-      "direction": "https://maps.app.goo.gl/NoLT99ttPn1Yqr969",
+      "direction": "Toma la ruta 125 desde la Terminal de Oriente en San Salvador. Al llegar a Chalatenango, puedes tomar un taxi que cobra $1 por persona o caminar de 10 a 20 minutos hasta el lugar.",
       "best_season_to_go": "Durante todo el año.",
       "where_to_stay": "Hotel y Restaurante LOS AMIGOS, La Posada de Suchitlán, Hotel y Restaurante El Tejado.",
       "where_to_eat": "Hotel y Restaurante LOS AMIGOS, Hotel y Restaurante El Tejado.",
@@ -65,7 +65,7 @@
     },
     {
       "place_id": 9,
-      "direction": "https://maps.app.goo.gl/F9XLjogCmeeHy3gi9",
+      "direction": "Toma la ruta de buses 119 desde la Terminal de Oriente en San Salvador. Esta ruta llega a la Frontera El Poy en Citalá y pasa por los municipios de La Palma y San Ignacio.",
       "best_season_to_go": "diciembre - abril.",
       "where_to_stay": "Hotel La Palma, Hostal Posada Real, Entre Pinos Hotel.",
       "where_to_eat": "La Placita Restaurant, Olan Café, Rocios Ice Cream.",
@@ -73,7 +73,7 @@
     },
     {
       "place_id": 10,
-      "direction": "https://maps.app.goo.gl/MiKuzJpkaGEE3CT3A",
+      "direction": "Toma la ruta de buses 119 desde la Terminal de Oriente en San Salvador. Esta ruta llega a la Frontera El Poy en Citalá y pasa por los municipios de La Palma y San Ignacio.",
       "best_season_to_go": "Durante todo el año.",
       "where_to_stay": "Entre Pinos Hotel, Las Tilapias Hotel y Restaurante, Cabañas El Pital.",
       "where_to_eat": "Las Tilapias Hotel y Restaurante, Restaurante Rancho Escondido, Romeros Cocina y Cafe.",
@@ -81,7 +81,7 @@
     },
     {
       "place_id": 11,
-      "direction": "https://maps.app.goo.gl/vPx2AqB7wrMMopwQ9",
+      "direction": "Toma la ruta de buses 119 desde la Terminal de Oriente en San Salvador; esta ruta va a la Frontera El Poy en Citalá y pasa por La Palma y San Ignacio. Si viajas en automóvil, toma la carretera a Santa Ana hacia Chalatenango y sigue las indicaciones hasta el parque acuático.",
       "best_season_to_go": "Durante todo el año.",
       "where_to_stay": "Hotel y Restaurante LOS AMIGOS, Las Terrazas del Alto, La Posada de Suchitlán.",
       "where_to_eat": "Hotel y Restaurante LOS AMIGOS, Restaurante El Paraíso, Hostal y Restaurante Paraíso de la montaña.",
