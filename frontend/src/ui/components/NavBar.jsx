497b230c638593c8268a0e16402b407ec7287ba6
MR211604 <elmastrollxd34@gmail.com>
2024-09-29 18:10:55 -0600

    frontend: actualizar ícono de la página principal y del navbar

--- frontend/src/ui/components/NavBar.jsx
+++ frontend/src/ui/components/NavBar.jsx
@@ -3,8 +3,9 @@ export const NavBar = () => {
     <>
       <nav className="bg-white dark:bg-gray-900 relative w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
+            <img src="../../public/icono-wonders-from-el-salvador.svg" className="h-8" alt="WFES Logo" />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white hidden lg:block">Wonders from El Salvador</span>
           </a>
           <div className="flex md:order-2 space-x-4 md:space-x-2 rtl:space-x-reverse">
